cd837d60577e2e905f809cbebbc220555dfa1ad9
zxcvtm <zxcvtm@gmail.com>
2022-03-02 00:46:44 -0300

    descarga de awscli

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,6 +16,10 @@ curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/
 chmod +x kubectl
 mv kubectl /usr/local/bin
 
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
 # Extract the base64 encoded config data and write this to the KUBECONFIG
 echo "$config" | base64 -d > /tmp/config
 export KUBECONFIG=/tmp/config
