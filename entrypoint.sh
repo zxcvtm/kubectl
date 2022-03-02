e68c77b009f883adefe1e415db0cfd25fa2f6bae
zxcvtm <zxcvtm@gmail.com>
2022-03-02 00:19:46 -0300

    change kubectl origin

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ fi
 
 echo "using kubectl@$version"
 
-curl -sLO "https://dl.k8s.io/release/$version/bin/linux/amd64/kubectl" -o kubectl
+curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/kubectl
 chmod +x kubectl
 mv kubectl /usr/local/bin
 
